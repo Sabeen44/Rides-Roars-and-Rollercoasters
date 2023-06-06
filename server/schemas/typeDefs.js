21e4a76006b84496e1cafb4b9ea063e96adcd10d
Beenz <CH.SABEEN@GMAIL.COM>
2023-06-06 12:09:56 -0700

    added TypeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,15 +2,32 @@ const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
   type Park {
-    
+    id: ID!
+    name: String
+  }
+
+  type Review {
+    id: ID!
+    title: String!
+    content: String!
+    rating: Int! #   user: User!}
+  }
+
+  type Auth {
+    token: ID!
+    user: User
   }
 
   type Query {
-    
+    me: User
   }
 
   type Mutation {
-   
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveReview(reviewData: reviewInput!): User
+    removeReview(reviewId: ID!): User
   }
 `;
+
 module.exports = typeDefs;
