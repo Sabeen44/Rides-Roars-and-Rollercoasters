5dfd9d8341b1496df31c412b7bf5d11c4d9ef645
Jordan Cardenas <jordanbc408@gmail.com>
2023-06-08 11:08:19 -0700

    add to typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -33,6 +33,8 @@ const typeDefs = gql`
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
     addComment(commentId: ID!, commentText: String!): Comment
+    savePark(parkData: parkInput!): User
+    removePark(parkId: ID!): User
     saveReview(reviewData: reviewInput!): User
     removeReview(reviewId: ID!): User
   }
