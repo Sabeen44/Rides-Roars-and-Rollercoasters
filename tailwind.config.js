fc9d5a60eb649c54b31f34de8c2ebe2abd9cb6b8
clapitan1 <cilapitan1@gmail.com>
2023-06-12 09:53:03 -0700

    tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["../src/index.css/"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
 
+module.exports = {
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      black: '#000',
+      white: '#fff',
+      gray: {
+        100: '#f7fafc',
+        900: '#1a202c',
+      },
+    }
+  }
+}
+
+module.exports = {
+  theme: {
+    borderRadius: {
+      'none': '0',
+      'sm': '.125rem',
+      DEFAULT: '.25rem',
+      'lg': '.5rem',
+      'full': '9999px',
+    },
+  }
+}
