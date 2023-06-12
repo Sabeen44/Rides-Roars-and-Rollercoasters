15aba059aed721a73821a380dd7778e28be40597
Beenz <CH.SABEEN@GMAIL.COM>
2023-06-12 15:24:54 -0700

    pushing to heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
+require("dotenv").config();
 mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/ridereviews'
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/ridereviews"
 );
 
 module.exports = mongoose.connection;
